1c1651c773edee724d8cdf386c010175343684fa
Stanislav <stanislav.khomyn@skylum.com>
2020-04-29 19:10:18 -0700

    Updates

---
+++ precache-manifest.f37c301b37be395676a86e38c36b7ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098591d65a0c6162956c2e7b44b72926",
+    "url": "/react-StarWars-app/index.html"
+  },
+  {
+    "revision": "19e79ecc2be1d0d7950f",
+    "url": "/react-StarWars-app/static/css/main.6d2b0a87.chunk.css"
+  },
+  {
+    "revision": "31b24c2558d3cb48a057",
+    "url": "/react-StarWars-app/static/js/2.9d26d60a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-StarWars-app/static/js/2.9d26d60a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e79ecc2be1d0d7950f",
+    "url": "/react-StarWars-app/static/js/main.0d490af9.chunk.js"
+  },
+  {
+    "revision": "9bc3c8d28d47cd6068f6",
+    "url": "/react-StarWars-app/static/js/runtime-main.e54fca00.js"
+  },
+  {
+    "revision": "e0d041e8bfa462c191a91f1ed27a2ed7",
+    "url": "/react-StarWars-app/static/media/placeholder.e0d041e8.jpg"
+  }
+]);
